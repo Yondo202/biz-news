5f32bc9cbfefca1d0299b5037a4978e4bcaa74c1
Yondo202 <yondooo61@gmail.com>
2020-10-06 01:29:47 -0700

    +

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout from '../../components/layout'
+import Layout from '../../components/Layout'
 import HomePar from '../../components/home'
 import axios from 'axios'
 import MainNews from '../../components/mainNews/mainNews'
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Head from 'next/head'
-
-import Layout from '../../components/layout'
+import Layout from '../../components/Layout'
 import MainNews from '../../components/mainNews/mainNews'
 import Index from '../../components/mainNews/indexNews'
 import { Container, Row, Col } from 'react-bootstrap'
